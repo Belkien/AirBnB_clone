4614a01bac981e17294508a4efde1dc863f57b9a
Belkien <belkienmasinde2@gmail.com>
2023-02-15 02:46:48 +0000

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,13 @@
     Define class FileStorage
 '''
 import json
-import models
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.review import Review
+from models.place import Place
+from models.amenity import Amenity
+from models.state import State
 
 
 class FileStorage:
