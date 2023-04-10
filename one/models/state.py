87a5a7d0ef11d4750c2df2f544bea072c0f34001
Belkien <belkienmasinde2@gmail.com>
2023-04-10 21:13:55 +0000

    belkien airbnb console new

--- one/models/state.py
+++ one/models/state.py
@@ -3,11 +3,10 @@
 from models.base_model import BaseModel
 
 
-class City(BaseModel):
-    '''Class for managing city objects'''
-    state_id = ""
+class State(BaseModel):
+    '''Class for managing state objects'''
     name = ""
 
     def __init__(self, *args, **kwargs):
-        '''Initializes attributes for the city class'''
+        '''Initializes attributes for the State class'''
         super().__init__(*args, **kwargs)
